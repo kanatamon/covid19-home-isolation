ad1abc2dd193f9ae373b90c2070599a1945faa75
kanatamon <ponde.moonlord@gmail.com>
2022-02-28 18:59:47 +0700

    feat(admin): add ability to write message to clipboard

--- app/components/home-isolation-form-smart-view.tsx
+++ app/components/home-isolation-form-smart-view.tsx
@@ -21,17 +21,56 @@ export const HomeIsolationFormSmartView: React.FC<
     setIsEditable(checked)
   }
 
+  const copyToClipboardHandler = () => {
+    if (!navigator.clipboard || !data.patients) {
+      return
+    }
+
+    const lines = [
+      `${data.zone}, ${data.address}`,
+      data.landmarkNote ? `จุดสังเกตุ ${data.landmarkNote}` : undefined,
+      ``,
+      `โทร. ${data.phone}`,
+      ``,
+      `รายชื่อผู้ป่วย`,
+      ...data.patients.map((patient, idx) => `${idx + 1}. ${patient.name}`),
+      ``,
+      `+++จำนวน ${data.patients.length} กล่อง+++`,
+      ``,
+      `https://www.google.com/maps/dir//${data.lat},${data.lng}`,
+    ]
+      .filter((line) => typeof line !== 'undefined')
+      .join('\n')
+
+    navigator.clipboard.writeText(lines).then(
+      function () {
+        console.log('Async: Copying to clipboard was successful!')
+      },
+      function (err) {
+        console.error('Async: Could not copy text: ', err)
+      }
+    )
+  }
+
   return (
     <div>
-      <div style={{ display: 'flex', gap: 4 }}>
-        <input
-          style={{ width: 'revert' }}
-          type="checkbox"
-          name="isEditable"
-          checked={isEditable}
-          onChange={isEditableChangeHandler}
-        />
-        <label>edit?</label>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ display: 'flex', gap: 4 }}>
+          <input
+            style={{ width: 'revert' }}
+            type="checkbox"
+            name="isEditable"
+            checked={isEditable}
+            onChange={isEditableChangeHandler}
+          />
+          <label>edit?</label>
+        </div>
+        <button
+          onClick={copyToClipboardHandler}
+          style={{ fontSize: '1em', width: 'max-content' }}
+        >
+          Copy
+        </button>
       </div>
       <hr style={{ margin: '0.75em 0 1.5em' }} />
       <p
