ac8e92acaf413dd938504c8813a97ae17ab1f675
kanatamon <ponde.moonlord@gmail.com>
2022-03-01 10:31:03 +0700

    refactor: isolate into hook

---
+++ app/hooks/useGetCurrentPosition.ts
@@ -0,0 +1,39 @@
+import * as React from 'react'
+
+type GetCurrentPositionState = 'idle' | 'pending' | 'success' | 'error'
+
+export const useGetCurrentPosition = () => {
+  const [state, setState] = React.useState<GetCurrentPositionState>('idle')
+  const [position, setPosition] = React.useState<google.maps.LatLngLiteral>()
+
+  const fetch = () => {
+    if (!navigator?.geolocation) {
+      console.warn(
+        `ERROR: 'window.navigator' is not supported on your browser.`
+      )
+      setState('error')
+      return
+    }
+    setState('pending')
+    navigator?.geolocation.getCurrentPosition(
+      (pos) => {
+        setState('success')
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        })
+      },
+      (error) => {
+        setState('error')
+        console.warn(`ERROR(${error.code}): ${error.message}`)
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
+    )
+  }
+
+  return { state, position, fetch }
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,6 +4,7 @@ import { Status, Wrapper } from '@googlemaps/react-wrapper'
 
 import { Map } from '~/components/map'
 import { HomeIsolationFormView } from '~/components/home-isolation-form-view'
+import { useGetCurrentPosition } from '~/hooks/useGetCurrentPosition'
 
 const INITIAL_ZOOM = 14
 const INITIAL_LAT_LNG = {
@@ -20,10 +21,7 @@ export default function Index() {
   const [editingMode, setEditingMode] = React.useState<EditingMode>(
     EditingMode.PinMap
   )
-
-  const [getCurrentPositionState, setGetCurrentPositionState] =
-    React.useState('idle')
-
+  const getCurrentPosition = useGetCurrentPosition()
   const [userMapPreferences, setUserMapPreferences] = React.useState<
     google.maps.LatLngLiteral & { zoom: number }
   >({
@@ -33,10 +31,16 @@ export default function Index() {
   const [markerLatLng, setMarkerLatLng] =
     React.useState<google.maps.LatLngLiteral>(INITIAL_LAT_LNG)
 
+  const { position } = getCurrentPosition
+  React.useEffect(
+    function updateUserMapPreferencesWhenGetCurrentPositionSuccess() {
+      position && setUserMapPreferences({ ...position, zoom: 17 })
+    },
+    [position]
+  )
+
   const onIdle = (markerLatLng: google.maps.LatLngLiteral | undefined) => {
-    if (markerLatLng) {
-      setMarkerLatLng(markerLatLng)
-    }
+    markerLatLng && setMarkerLatLng(markerLatLng)
   }
 
   // const geolocationPlace = (
@@ -80,38 +84,15 @@ export default function Index() {
       </div>
     )
   } else if (editingMode === EditingMode.PinMap) {
-    const getCurrentPositionHandler = () => {
-      setGetCurrentPositionState('pending')
-      navigator?.geolocation.getCurrentPosition(
-        (pos) => {
-          setGetCurrentPositionState('success')
-          setUserMapPreferences({
-            lat: pos.coords.latitude,
-            lng: pos.coords.longitude,
-            zoom: 18,
-          })
-        },
-        (error) => {
-          setGetCurrentPositionState('error')
-          console.warn(`ERROR(${error.code}): ${error.message}`)
-        },
-        {
-          enableHighAccuracy: true,
-          timeout: 5000,
-          maximumAge: 0,
-        }
-      )
-    }
-
     editor = (
       <div>
         {/* {geolocationPlace}
         <div style={{ height: 24 }} /> */}
         <button
-          onClick={getCurrentPositionHandler}
-          disabled={getCurrentPositionState === 'pending'}
+          onClick={getCurrentPosition.fetch}
+          disabled={getCurrentPosition.state === 'pending'}
         >
-          {getCurrentPositionState === 'pending'
+          {getCurrentPosition.state === 'pending'
             ? 'กำลังค้นหา...'
             : 'ตำแหน่งปัจจุบัน'}
         </button>
@@ -149,11 +130,8 @@ export default function Index() {
           render={render}
         >
           <Map
-            // center={center}
             canInteract={canPinMap}
             userPreferences={userMapPreferences}
-            // initialLatLng={INITIAL_LAT_LNG}
-            // initialZoom={INITIAL_ZOOM}
             onIdle={onIdle}
             style={{ width: '100%', height: '100%' }}
             fullscreenControl={false}
