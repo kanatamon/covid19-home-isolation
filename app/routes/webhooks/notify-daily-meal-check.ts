b5f23e7803b445e91e199d0cccfbdccd52147df9
kanatamon <ponde.moonlord@gmail.com>
2022-03-30 23:09:35 +0700

    feat: add webhook to notify meal check

---
+++ app/routes/webhooks/notify-daily-meal-check.ts
@@ -0,0 +1,73 @@
+import { ActionFunction, json } from 'remix'
+import { serverError } from 'remix-utils'
+import { TextMessage } from '@line/bot-sdk'
+
+import { db } from '~/utils/db.server'
+import { activeTreatmentPeriod } from '~/domain/treatment'
+import { lineClient } from '~/utils/line-client.server'
+import { requireWebhookSignature } from '~/utils/webhook.server'
+import {
+  contactSchema,
+  Contact,
+  ContactsFetcher,
+  genNowDisplayNotifyTime,
+} from '~/domain/notify-message.server'
+import moment from 'moment'
+
+export const action: ActionFunction = async ({ request }) => {
+  await requireWebhookSignature(request)
+
+  const toNotifyContacts: Contact[] = await contactFetcher()
+
+  const notifyMessageRequests = toNotifyContacts.map(async (contact) => {
+    const displayNotifyTime = genNowDisplayNotifyTime()
+    const message: TextMessage = {
+      type: 'text',
+      text: `${displayNotifyTime} ขอให้ ผู้รักษาตัว คุณ${contact.lineDisplayName} ส่งรูป อาหารกลางวัน มาด้วยนะคะ`,
+      quickReply: {
+        items: [
+          {
+            type: 'action',
+            action: {
+              type: 'cameraRoll',
+              label: 'ส่งรูป',
+            },
+          },
+        ],
+      },
+    }
+
+    return lineClient.pushMessage(contact.lineId, message)
+  })
+
+  await Promise.allSettled(notifyMessageRequests)
+
+  return json({})
+}
+
+const contactFetcher: ContactsFetcher = async () => {
+  const contacts = await db.homeIsolationForm.findMany({
+    select: {
+      lineId: true,
+      lineDisplayName: true,
+      admittedAt: true,
+    },
+    where: {
+      admittedAt: {
+        gte: activeTreatmentPeriod.getDateSinceFirstDay(1),
+        lt: moment().startOf('day').toDate(),
+      },
+      NOT: { lineId: null },
+    },
+  })
+
+  const parseResult = contactSchema.array().safeParse(contacts)
+  if (!parseResult.success) {
+    throw serverError({
+      message: 'Oops! contacts fetching is received unexpected',
+      error: parseResult.error,
+    })
+  }
+
+  return parseResult.data
+}
